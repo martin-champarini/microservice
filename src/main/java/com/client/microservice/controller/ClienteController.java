2f49342597ef371bf693233ecf2467c82d156c42
martin champarini <martin.champarini.dev@gmail.com>
2021-01-31 14:58:09 -0300

    added swagger cfg

--- src/main/java/com/client/microservice/controller/ClienteController.java
+++ src/main/java/com/client/microservice/controller/ClienteController.java
@@ -4,9 +4,7 @@ import com.client.microservice.Model.Cliente;
 import com.client.microservice.service.ClienteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
@@ -18,7 +16,7 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
-    @GetMapping("/kpideclientes")
+    @GetMapping(path = "/kpideclientes")
     public ResponseEntity<Cliente> getKpiDeClientes(@RequestParam String nombre) throws ParseException {
         Cliente cliente = clienteService.getCliente(nombre);
         return new ResponseEntity<>(cliente, HttpStatus.OK);
